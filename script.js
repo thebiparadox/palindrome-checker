1ca1eef841276f2b31d9b9e2388477e9abf31630
Tessa <thebiparadox@gmail.com>
2024-03-01 13:22:31 -0500

    Created HTML and notes for JS

---
+++ script.js
@@ -0,0 +1,19 @@
+//returns true if str is palindrome
+//returns false if str is not palindrome
+function isPalindrome(str) {
+
+// Step 1: make str lower case
+const lowerCaseString = str.toLowerCase;
+
+// Step 2: Clean up str
+// Step 3: Split str into individual letters
+// Step 4: Reverse the order of the letters
+// Step 5: Join the letters back into one str
+// Step 6: Compare that reversed str is the same as the cleaned up str
+
+console.log(lowerCaseString)
+
+return IsSame;
+}
+
+isPalindrome("Racecar");
