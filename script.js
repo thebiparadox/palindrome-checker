529b4b39ae8dc976e7802ad97cdd3a908ce6a413
Tessa <thebiparadox@gmail.com>
2024-03-01 14:17:15 -0500

    added steps to JS file

--- script.js
+++ script.js
@@ -2,18 +2,30 @@
 //returns false if str is not palindrome
 function isPalindrome(str) {
 
-// Step 1: make str lower case
-const lowerCaseString = str.toLowerCase;
-
-// Step 2: Clean up str
-// Step 3: Split str into individual letters
-// Step 4: Reverse the order of the letters
-// Step 5: Join the letters back into one str
-// Step 6: Compare that reversed str is the same as the cleaned up str
-
-console.log(lowerCaseString)
-
-return IsSame;
-}
-
-isPalindrome("Racecar");
+    // Step 1: make str lower case
+    const lowerCaseString = str.toLowerCase();
+    
+    // Step 2: Clean up str
+    const regex = /[\W_]/g;
+    const cleanedUpString = lowerCaseString.replace(regex, "");
+    // Step 3: Split str into individual letters
+    const splitString = cleanedUpString.split("");
+    // Step 4: Reverse the order of the letters
+    const reversedLetters = splitString.reverse();
+    // Step 5: Join the letters back into one str
+    const reversedString = reversedLetters.join("");
+    // Step 6: Compare that reversed str is the same as the cleaned up str
+    const isSame = reversedString === cleanedUpString;
+    // Step 7: get input elements from DOM
+    const input = document.getElementById("text-input");
+    const button = document.getElementById("check-btn");
+    // Step 8: add eventlistener
+    button.addEventListener("click", () => {});
+    // Step 9: get string from input
+    // Step 10: pass string through steps 1-6
+    // Step 11: Display result in result div
+    
+    //return IsSame;
+    }
+    
+    isPalindrome("TaC$$$%*ocAT");
