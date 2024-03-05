69a7d85f9ed20801a8798b29269dfe054fb680b7
thebiparadox <147628347+thebiparadox@users.noreply.github.com>
2024-03-05 11:16:18 -0500

    refactored functions for simplicity

--- script.js
+++ script.js
@@ -1,34 +1,23 @@
-//returns true if str is palindrome
-//returns false if str is not palindrome
-const userInput = document.getElementById("#text-input");
-function isPalindrome(input) {
-    // Step 1: make str lower case
-    const lowerCaseString = input.toLowerCase();
-    // Step 2: Clean up str
-    const regex = /[\W_]/g;
-    const cleanedUpString = lowerCaseString.replace(regex, "");
-    // Step 3: Split str into individual letters
-    const splitString = cleanedUpString.split("");
-    // Step 4: Reverse the order of the letters
-    const reversedLetters = splitString.reverse();
-    // Step 5: Join the letters back into one str
-    const reversedString = reversedLetters.join("");
-    // Step 6: Compare that reversed str is the same as the cleaned up str
-    const isSame = reversedString === cleanedUpString;
-    //return IsSame;
-    }
-
-const resultText = document.getElementById("#result");
-const showResults = `strong${userInput}strong is ${isPalindrome}`;
-
-    
+const userInput = document.getElementById("text-input");
 const button = document.getElementById("check-btn");
+const resultMsg = document.getElementById("result-div");
 
-button.addEventListener("click", () => {
-    isPalindrome(userInput.value);
-    userInput.value = "";
-    })
 
-const pTag = document.createElement('p');
-pTag.className = 'user-input';
-pTag.innerHTML = showResults;
+const formatInput = (str) => {
+    const string = userInput.value.toString;
+    const lowerCaseString = string.toLowerCase();
+    const regex = /[\W_]/g;
+    return lowerCaseString.replace(regex, "");
+}
+
+const result = () => {
+    const formattedInput = formatInput();
+    const reversedInput = formattedInput.split("").reverse().join("");
+    if (reversedInput === formattedInput) {
+        console.log("is palindrome")
+    } else {
+        console.log("is not palindrome")
+    }
+}
+
+button.addEventListener("click", result)
