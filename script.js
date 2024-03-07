56c2a71ba6a1cfff2bb6840264275b76674c345d
Tessa <147628347+thebiparadox@users.noreply.github.com>
2024-03-07 13:02:28 -0500

    Update script.js

--- script.js
+++ script.js
@@ -2,22 +2,22 @@ const userInput = document.getElementById("text-input");
 const button = document.getElementById("check-btn");
 const resultMsg = document.getElementById("result-div");
 
-
 const formatInput = (str) => {
-    const string = userInput.value.toString;
-    const lowerCaseString = string.value.toLowerCase();
-    const regex = /[\W_]/g;
-    return lowerCaseString.value.replace(regex, "");
-}
+  const string = userInput.value.toString();
+  const lowerCaseString = string.toLowerCase();
+  const regex = /[\W_]/g;
+  return lowerCaseString.replace(regex, "");
+};
 
 function result() {
-    const formattedInput = formatInput();
-    const reversedInput = formattedInput.split("").reverse().join("");
-    if (reversedInput === formattedInput) {
-        console.log("is palindrome");
-    } else {
-        console.log("is not palindrome");
-    }
+  const formattedInput = formatInput();
+  const reversedInput = formattedInput.split("").reverse().join("");
+  if (reversedInput === formattedInput) {
+    resultMsg.innerText += `${userInput.value} is a palindrome.`;
+  } else {
+    resultMsg.innerText += `${userInput.value} is not a palindrome.`;
+  }
 }
 
-button.addEventListener("click", result)
+button.addEventListener("click", result());
+
