425e24b5b172b36634a6475428c223a155a4371c
Tessa <147628347+thebiparadox@users.noreply.github.com>
2024-03-12 15:25:02 -0400

    Updated script.js
    Added a function that alerts the user to a blank input

--- script.js
+++ script.js
@@ -1,29 +1,38 @@
+// get objects from DOM
 const userInput = document.getElementById("text-input");
 const button = document.getElementById("check-btn");
 const resultMsg = document.getElementById("result-div");
 
-function checkUserInput {
-  if (!userInput) {
+// format input
+const formatInput = (str) => {
+ 
+// if input is blank, alert user
+  if (userInput.value === "") {
     alert("Please provide word or phrase");
     return;
-  }
+  } else {
 
-const formatInput = (str) => {
+// converts input to lowercase string and removes all non-alphanumeric characters
   const string = userInput.value.toString();
   const lowerCaseString = string.toLowerCase();
   const regex = /[\W_]/g;
   return lowerCaseString.replace(regex, "");
+  }
 };
 
+// checks if palindrome
 function result() {
+// gets formatted input and reverses it
   const formattedInput = formatInput();
   const reversedInput = formattedInput.split("").reverse().join("");
+  
+// checks if reversed input is the same as formatted input
   if (reversedInput === formattedInput) {
     resultMsg.innerText += `${userInput.value} is a palindrome.`;
   } else {
     resultMsg.innerText += `${userInput.value} is not a palindrome.`;
   }
-}
+  }
 
 button.addEventListener("click", result);
 userInput.addEventListener("keydown", (e) => {
